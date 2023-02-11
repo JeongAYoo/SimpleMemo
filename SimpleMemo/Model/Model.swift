8ceda16800c51a3dbb5dbd2f76ba69a251d15859
JeongAYoo <jeonga038@naver.com>
2023-02-12 06:33:29 +0900

    TableView, TableViewCell

---
+++ SimpleMemo/Model/Model.swift
@@ -0,0 +1,23 @@
+//
+//  Model.swift
+//  SimpleMemo
+//
+//  Created by Jade Yoo on 2023/02/12.
+//
+
+import Foundation
+
+class Memo {
+    var content: String
+    var insertDate: Date
+    
+    init(content: String) {
+        self.content = content
+        self.insertDate = Date()
+    }
+    
+    static var dummyMemoList = [
+        Memo(content: "Lorem Ipsum"),
+        Memo(content: "Subscribe + 👍 = ❤️")
+    ]
+}
